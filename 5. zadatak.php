f1713969e813d606aba8128aa513fcb5a2de159c
Roman Šimunović <67165057+romanuspopulsque@users.noreply.github.com>
2025-04-09 14:36:47 +0200

    Update 5. zadatak.php

--- 5. zadatak.php
+++ 5. zadatak.php
@@ -1,16 +1,18 @@
 <?php
-function sl_datum($startDate, $endDate) {
-    // Pretvori početni i krajnji datum u timestamp
-    $startTimestamp = strtotime($startDate);
-    $endTimestamp = strtotime($endDate);
 
-    // Generiraj slučajan timestamp između startTimestamp i endTimestamp
-    $randomTimestamp = rand($startTimestamp, $endTimestamp);
+function sl_datum($pocetak, $kraj) {
+    // Pretvori datume u UNIX timestamp
+    $start = strtotime($pocetak);
+    $end = strtotime($kraj);
 
-    // Pretvori random timestamp u datum u željenom formatu
-    return date('j.n.Y', $randomTimestamp); // Format d.m.G
+    // Generiraj slučajni timestamp između početka i kraja
+    $randomTimestamp = rand($start, $end);
+
+    // Vrati datum u formatu d.m.Y.
+    return date("j.n.Y.", $randomTimestamp);
 }
 
-// Primjer upotrebe:
+// Primjer poziva
 echo sl_datum("2023-06-01", "2024-06-26");
+
 ?>
